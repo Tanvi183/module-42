2283bb95c6ad955c2e75d7e51fd006b6683fbced
tanvi183 <mneshat7@gmail.com>
2025-09-29 00:00:10 -0700

    basic setup-tailwind,deiseUi,lucideIcons

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Camera } from 'lucide-react';
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='text-center'>Vite + React</h1>
+      <Camera color="red" size={48} />
+
+    </>
+  )
+}
+
+export default App
