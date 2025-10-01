12b98e05003eace6dbcaf2aa563ada09da6e6792
tanvi183 <mneshat7@gmail.com>
2025-09-30 23:47:22 -0700

    axios use for data loading nested data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import PricingOptions from './Compnents/PricingOptoins/PricingOptions';
 import ResultChart from './Compnents/LineChart/ResultChart'
 import Navbar from './Compnents/Navbar/Navbar'
 import ProductBarChart from './Compnents/BarChart/ProductBarChart';
+import axios from 'axios';
+import ExamMarks from './Compnents/ExamMarks/ExamMarks';
 
 const pricingPromise = fetch('pricingData.json').then(res => res.json());
 const marksPrimise = fetch('../public/studentMark.json').then(res => res.json());
+const exmaMarksPromise = axios.get('examMarks.json');
 
 function App() {
 
@@ -15,7 +18,9 @@ function App() {
 
       {/* <ResultChart marksPrimise={marksPrimise}></ResultChart> */}
 
-      <ProductBarChart></ProductBarChart>
+      {/* <ProductBarChart></ProductBarChart> */}
+
+      <ExamMarks exmaMarksPromise={exmaMarksPromise}></ExamMarks>
 
       <PricingOptions pricingPromise={pricingPromise}></PricingOptions>
 
